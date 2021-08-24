aa35395bbc6e6a042720affc3d9b55e231c95045
landrek <kurt.landre@autodesk.com>
2021-08-24 10:26:28 -0700

    Pushing data files for Renewal type tests...

--- src/main/java/com/autodesk/pws/test/steps/base/SeleniumActionBase.java
+++ src/main/java/com/autodesk/pws/test/steps/base/SeleniumActionBase.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
-//import io.github.bonigarcia.wdm.WebDriverManager
 
 public class SeleniumActionBase extends StepBase
 {
