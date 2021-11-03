a5b74b1aed66a7c1b456163d01075d7d69e62ff4
Prateek Parab <pratik.parab@autodesk.com>
2021-11-03 15:23:29 -0700

    Added initial files for the S2S wpe workflow

--- src/main/java/com/autodesk/pws/test/workflow/WorkflowLibrary.java
+++ src/main/java/com/autodesk/pws/test/workflow/WorkflowLibrary.java
@@ -141,4 +141,24 @@ public class WorkflowLibrary
 
         return workflow;
     }
+
+
+    public static List<StepBase> PlaceS2SOrder()
+    {
+    	 //  Initial PlaceOrder...
+    	 List<StepBase> workflow = PlaceOrder();
+    	 
+    	 //  AddOn order...
+         workflow.add(new LoadAddOnFilesAndExtractAddOnInfoData());
+         workflow.add(new GetOAuthCredentials());
+         workflow.add(new WaitForGetAssetDetails());
+         workflow.add(new GetSkuPrice());
+         workflow.add(new PostOrder());
+         workflow.add(new GetOAuthCredentials());
+         workflow.add(new WaitForOrderStatusChange());
+         workflow.add(new GetOrderDetailsV1());
+         workflow.add(new WaitForGetAgreementInfo());
+         
+    	 return workflow;
+    }
 }
