9c807d20fea388693e475fc3430f6db1198cd3c3
rossib <brandon.rossi@autodesk.com>
2021-12-16 16:52:30 -0800

    Format Java

--- src/main/java/com/autodesk/pws/test/workflow/Workflows.java
+++ src/main/java/com/autodesk/pws/test/workflow/Workflows.java
@@ -6,25 +6,22 @@ import java.util.*;
 
 import com.autodesk.pws.test.steps.base.*;
 
-public class Workflows
-{
-    @SuppressWarnings("unchecked")
-	public List<StepBase> getWorkflowByName(String workflowName) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException
-    {
-    	List<StepBase> retVal = new ArrayList<StepBase>();
+public class Workflows {
+  @SuppressWarnings("unchecked")
+  public List<StepBase> getWorkflowByName(String workflowName)
+      throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+    List<StepBase> retVal = new ArrayList<StepBase>();
 
-        Method[] methods = WorkflowLibrary.class.getMethods();
-        WorkflowLibrary workflowLibraryObject = new WorkflowLibrary();
+    Method[] methods = WorkflowLibrary.class.getMethods();
+    WorkflowLibrary workflowLibraryObject = new WorkflowLibrary();
 
-        for (int i = 0; i < methods.length; i++)
-        {
-            if(methods[i].getName().toLowerCase().compareTo(workflowName.toLowerCase()) == 0)
-            {
-                retVal = (List<StepBase>) methods[i].invoke(workflowLibraryObject);
-                break;
-            }
-        }
-
-        return retVal;
+    for (int i = 0; i < methods.length; i++) {
+      if (methods[i].getName().toLowerCase().compareTo(workflowName.toLowerCase()) == 0) {
+        retVal = (List<StepBase>) methods[i].invoke(workflowLibraryObject);
+        break;
+      }
     }
+
+    return retVal;
+  }
 }
