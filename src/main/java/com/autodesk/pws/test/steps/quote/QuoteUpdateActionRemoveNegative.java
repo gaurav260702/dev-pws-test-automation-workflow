f815584e4ff17a35f187bf1195302d4258aaba54
pathads <pathads@ADSKPF3TJLM2>
2023-06-16 19:41:16 +0530

    Added update quote test cases both positive and negative on INT

---
+++ src/main/java/com/autodesk/pws/test/steps/quote/QuoteUpdateActionRemoveNegative.java
@@ -0,0 +1,100 @@
+package com.autodesk.pws.test.steps.quote;
+
+import com.autodesk.pws.test.steps.base.PwsServiceBase;
+import io.restassured.path.json.JsonPath;
+
+public class QuoteUpdateActionRemoveNegative extends PwsServiceBase
+{
+	public String DataPoolSourceInfoLabel = "";
+	
+    @Override
+    public void preparation()
+    {
+		//  Do some basic variable preparation...
+    	this.UseAlternateAuthHeaderGenerationMethod = true;
+    	
+    	//  Need to set the ClassName here as this will be
+    	//  used by the super/base classes ".preparation()" 
+    	//  method.
+		this.ClassName = this.getClass().getSimpleName();
+		this.DataPoolSourceInfoLabel = "raw" + this.ClassName;
+		
+		//  Set the ServiceVerb to a "PATCH" style service.
+		//  We're doing this first in case there are any 
+		//  init methods that may have a dependency on it.
+		//  Naturally, we can't allow any 'setAs***Service()'
+		//  methods to have any dependencies if it's being
+		//  called before anything else...
+		
+		
+		//  Have some crappy fall-through code here, but this
+		//  should only be temporary until the STG environment
+		//  gets updated to the same code base as INT environment...
+		this.setServiceVerb("PUT");
+		
+		//  Set the Resource path BEFORE the base/super class
+		//  sets the targetUrl during the super class's
+		//  "preparation()" method..
+		setResourcePath();
+
+    	//  Do stuff that the Action depends on to execute...
+    	super.preparation();
+    	
+    	ExpectedEndStateStatus = "error";
+		super.setExpectedEndState(this.ClassName);
+    	
+    	//  Having a "PATCH" method requires a body.
+    	//  This should be stored in an external file, but
+    	//  for the moment we're going to embed it in the 
+    	//  class as I don't want to deal with creating a
+    	//  loader/extracter at this time...
+    //	String jsonBody = "{\"quoteNumber\":\"$QUOTE_NUMBER$\"}";
+		String jsonBody = "{\n" +
+				"    \"quoteNumber\": \"Q-231770\",\n" +
+				"    \"lineItems\": [{\n" +
+				"        \"action\": \"remove\",\n" +
+				"        \"quoteLineNumber\": \"QL-0247829\"\n" +
+				"    }]\n" +
+				"}";
+
+    	jsonBody = this.fullyDetokenize(jsonBody);
+    	
+    	this.setJsonRequestBody(jsonBody);
+    	
+    	setTargetUrl();
+    }
+
+    private void setResourcePath()
+    {
+		super.setResourcePath("/v1/quotes");
+    }
+
+	@Override
+    public void action()
+    {
+		// attachHeaderFromDataPool("CSN", "$CSN_SECONDARY$");
+		
+		super.action();
+    }
+	
+	@Override
+	public void validation()
+	{    	
+		super.validation();
+		
+		//  :::TODO:::
+		//  Need to abstract this whole mechanism into the "RestActionBase" 
+		//  or the "PwsServiceBase" set of methods and properties...
+		String json = this.JsonResponseBody;
+		JsonPath pathFinder = JsonPath.from(json);
+		String finalStatus = pathFinder.get("status");
+
+		if (!finalStatus.matches(ExpectedEndStateStatus)) 
+		{
+			this.addResponseToValidationChain();
+			ExceptionAbortStatus = true;
+			ExceptionMessage = "Expected to reach '" + ExpectedEndStateStatus + "' state, but ended in '" + finalStatus + "' state!";
+		}
+		//  :::TODO:::
+	}	
+}
--- src/main/java/com/autodesk/pws/test/workflow/WorkflowLibrary.java
+++ src/main/java/com/autodesk/pws/test/workflow/WorkflowLibrary.java
@@ -325,4 +325,80 @@ public class WorkflowLibrary
 
         return workflow;
     }
+
+    public static List<StepBase> QuoteUpdateActionUpdatePositive()
+    {
+        boolean waitForExpectedStatus = true;
+
+        List<StepBase> workflow = new ArrayList<StepBase>();
+
+        workflow.add(new LoadQuoteFilesAndExtractData());
+        workflow.add(new GetQuoteOAuthCredentials());
+        workflow.add(new QuoteCreate());
+        workflow.add(new QuoteStatus(waitForExpectedStatus));
+        //  workflow.add(new QuoteDetails());
+        //  workflow.add(new QuoteFinalize());
+        //  workflow.add(new QuoteStatusByQuoteNumber(waitForExpectedStatus));
+        workflow.add(new QuoteUpdateActionUpdatePositive());
+
+
+        return workflow;
+    }
+
+    public static List<StepBase> QuoteUpdateActionRemoveLineNumberMismatchedNeg()
+    {
+        boolean waitForExpectedStatus = true;
+
+        List<StepBase> workflow = new ArrayList<StepBase>();
+
+        workflow.add(new LoadQuoteFilesAndExtractData());
+        workflow.add(new GetQuoteOAuthCredentials());
+        workflow.add(new QuoteCreate());
+        workflow.add(new QuoteStatus(waitForExpectedStatus));
+        workflow.add(new QuoteDetails());
+        workflow.add(new QuoteFinalize());
+        workflow.add(new QuoteStatusByQuoteNumber(waitForExpectedStatus));
+        workflow.add(new QuoteUpdateLineNumberMismatchedNeg());
+
+
+        return workflow;
+    }
+
+    public static List<StepBase> QuoteUpdateActionRemoveNegative()
+    {
+        boolean waitForExpectedStatus = true;
+
+        List<StepBase> workflow = new ArrayList<StepBase>();
+
+        workflow.add(new LoadQuoteFilesAndExtractData());
+        workflow.add(new GetQuoteOAuthCredentials());
+        workflow.add(new QuoteCreate());
+        workflow.add(new QuoteStatus(waitForExpectedStatus));
+        workflow.add(new QuoteDetails());
+        workflow.add(new QuoteFinalize());
+        workflow.add(new QuoteStatusByQuoteNumber(waitForExpectedStatus));
+        workflow.add(new QuoteUpdateActionRemoveNegative());
+
+
+        return workflow;
+    }
+
+    public static List<StepBase> QuoteUpdateActionRemovePositive()
+    {
+        boolean waitForExpectedStatus = true;
+
+        List<StepBase> workflow = new ArrayList<StepBase>();
+
+        workflow.add(new LoadQuoteFilesAndExtractData());
+        workflow.add(new GetQuoteOAuthCredentials());
+        workflow.add(new QuoteCreate());
+        workflow.add(new QuoteStatus(waitForExpectedStatus));
+        //  workflow.add(new QuoteDetails());
+        //  workflow.add(new QuoteFinalize());
+        //  workflow.add(new QuoteStatusByQuoteNumber(waitForExpectedStatus));
+        workflow.add(new QuoteUpdateActionRemovePositive());
+
+
+        return workflow;
+    }
 }
