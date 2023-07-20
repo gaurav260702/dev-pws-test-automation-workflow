27dad865a4534f5b87c6bdfe08a242d65eae4295
pathads <pathads@ADSKPF3TJLM2>
2023-07-20 18:04:09 +0530

    Adding a new test case for Create Quote V2 where Is Individual = True Negative case on INT

--- src/main/java/com/autodesk/pws/test/workflow/WorkflowLibrary.java
+++ src/main/java/com/autodesk/pws/test/workflow/WorkflowLibrary.java
@@ -201,6 +201,21 @@ public class WorkflowLibrary
         return workflow;
     }
 
+    public static List<StepBase> CreateQuoteV2NewIsIndividualTrueNegative()
+    {
+        boolean waitForExpectedStatus = true;
+
+        List<StepBase> workflow = new ArrayList<StepBase>();
+
+        workflow.add(new LoadQuoteFilesAndExtractData());
+        workflow.add(new GetQuoteOAuthCredentials());
+        workflow.add(new QuoteCreate());
+       // workflow.add(new QuoteCreateV2New());
+
+
+        return workflow;
+    }
+
 
     public static List<StepBase> CreateQuoteResendEmail()
     {
