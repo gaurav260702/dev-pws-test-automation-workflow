0b85ea520193897a6b5d29cc6dcac5a5f7792dd1
pathads <pathads@ADSKPF3TJLM2>
2023-09-04 12:16:54 +0530

    Added Delete tc for Quote Services v2

--- src/main/java/com/autodesk/pws/test/workflow/WorkflowLibrary.java
+++ src/main/java/com/autodesk/pws/test/workflow/WorkflowLibrary.java
@@ -201,6 +201,25 @@ public class WorkflowLibrary
         return workflow;
     }
 
+    public static List<StepBase> UpdateQuoteV2OperationDelete()
+    {
+        boolean waitForExpectedStatus = true;
+
+        List<StepBase> workflow = new ArrayList<StepBase>();
+
+        workflow.add(new LoadQuoteFilesAndExtractData());
+        workflow.add(new GetQuoteOAuthCredentials());
+        workflow.add(new QuoteCreate());
+        // workflow.add(new QuoteCreateV2New());
+        workflow.add(new QuoteStatus(waitForExpectedStatus));
+        workflow.add(new QuoteDetailsV2New());
+        workflow.add(new QuoteDeleteV2());
+    //  workflow.add(new QuoteFinalizeV2New());
+    //  workflow.add(new QuoteStatusByQuoteNumber(waitForExpectedStatus));
+
+        return workflow;
+    }
+
     public static List<StepBase> CreateQuoteV2NewMOAB()
     {
         boolean waitForExpectedStatus = true;
