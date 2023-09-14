90de8529045d8997c1f30bf69d63a1102de4cc11
pathads <pathads@ADSKPF3TJLM2>
2023-09-14 21:59:46 +0530

    Updated error code for create quote v2 test cases on INT

--- src/main/java/com/autodesk/pws/test/workflow/WorkflowLibrary.java
+++ src/main/java/com/autodesk/pws/test/workflow/WorkflowLibrary.java
@@ -201,6 +201,23 @@ public class WorkflowLibrary
         return workflow;
     }
 
+    public static List<StepBase> CreateQuoteV2Extension()
+    {
+        boolean waitForExpectedStatus = true;
+
+        List<StepBase> workflow = new ArrayList<StepBase>();
+
+        workflow.add(new LoadQuoteFilesAndExtractData());
+        workflow.add(new GetQuoteOAuthCredentials());
+        workflow.add(new QuoteCreate());
+        // workflow.add(new QuoteCreateV2New());
+        workflow.add(new QuoteStatus(waitForExpectedStatus));
+        workflow.add(new QuoteDetailsV2New());
+
+
+        return workflow;
+    }
+
     public static List<StepBase> RenewalQuoteV2New()
     {
         boolean waitForExpectedStatus = true;
