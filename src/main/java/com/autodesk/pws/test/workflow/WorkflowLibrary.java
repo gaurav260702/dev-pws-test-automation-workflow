a3adac6a3b5012a239f3cec40d3be3ea171f9e66
pathads <pathads@ADSKPF3TJLM2>
2023-10-26 18:46:29 +0530

    Added new test case Kicker.QuoteServices.Update.Quote.V2.Operation.Update.Action.Extension.EndDate.Promocode.Positive.STG.json

--- src/main/java/com/autodesk/pws/test/workflow/WorkflowLibrary.java
+++ src/main/java/com/autodesk/pws/test/workflow/WorkflowLibrary.java
@@ -289,6 +289,23 @@ public class WorkflowLibrary
         return workflow;
     }
 
+    public static List<StepBase> QuoteUpdateOperationUpdateActionExtension()
+    {
+        boolean waitForExpectedStatus = true;
+
+        List<StepBase> workflow = new ArrayList<StepBase>();
+
+        workflow.add(new LoadQuoteFilesAndExtractData());
+        workflow.add(new GetQuoteOAuthCredentials());
+        workflow.add(new QuoteCreate());
+        // workflow.add(new QuoteCreateV2New());
+        workflow.add(new QuoteStatus(waitForExpectedStatus));
+        workflow.add(new QuoteDetailsV2New());
+        workflow.add(new QuoteUpdateOperationUpdateActionExtension());
+
+        return workflow;
+    }
+
     public static List<StepBase> CreateQuoteV2Extension()
     {
         boolean waitForExpectedStatus = true;
