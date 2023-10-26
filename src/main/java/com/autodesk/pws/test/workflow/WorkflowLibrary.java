3badeb9bb8b062e620b1c126037b5ab87997d9c0
pathads <pathads@ADSKPF3TJLM2>
2023-10-26 16:24:30 +0530

    Added new test case Kicker.QuoteServices.Update.Quote.V2.Operation.Update.Action.New.Quantity.Positive.STG.json

--- src/main/java/com/autodesk/pws/test/workflow/WorkflowLibrary.java
+++ src/main/java/com/autodesk/pws/test/workflow/WorkflowLibrary.java
@@ -221,6 +221,23 @@ public class WorkflowLibrary
         return workflow;
     }
 
+    public static List<StepBase> QuoteUpdateOperationUpdateNew()
+    {
+        boolean waitForExpectedStatus = true;
+
+        List<StepBase> workflow = new ArrayList<StepBase>();
+
+        workflow.add(new LoadQuoteFilesAndExtractData());
+        workflow.add(new GetQuoteOAuthCredentials());
+        workflow.add(new QuoteCreate());
+        // workflow.add(new QuoteCreateV2New());
+        workflow.add(new QuoteStatus(waitForExpectedStatus));
+        workflow.add(new QuoteDetailsV2New());
+        workflow.add(new QuoteUpdateOperationUpdateNew());
+
+        return workflow;
+    }
+
     public static List<StepBase> CreateQuoteV2Extension()
     {
         boolean waitForExpectedStatus = true;
