7c360fafb11f6ec2ccc7c5868f82bdb88ff83d57
pathads <pathads@ADSKPF3TJLM2>
2023-11-09 22:01:49 +0530

    Added new test case for Kicker.QuoteServices.Update.Quote.V2.Operation.Delete.Action.New.Not.In.Draft.State.Negative.STG.json

--- src/main/java/com/autodesk/pws/test/workflow/WorkflowLibrary.java
+++ src/main/java/com/autodesk/pws/test/workflow/WorkflowLibrary.java
@@ -200,9 +200,6 @@ public class WorkflowLibrary
         workflow.add(new QuoteDetailsV2New());
         workflow.add(new QuoteFinalizeV2New());
         workflow.add(new QuoteStatusByQuoteNumber(waitForExpectedStatus));
-        workflow.add(new GetQuoteDetailsInternalv2ForgeCredentials());
-        workflow.add(new QuoteDetailsInternalv2_quoteNumber());
-
         return workflow;
     }
 
@@ -236,6 +233,26 @@ public class WorkflowLibrary
         // workflow.add(new QuoteCreateV2New());
         workflow.add(new QuoteStatus(waitForExpectedStatus));
         workflow.add(new QuoteDetailsV2New());
+        workflow.add(new QuoteUpdateOperationDelete());
+
+        return workflow;
+    }
+
+    public static List<StepBase> QuoteUpdateOperationDeleteQuoteNotInDraftStateNeg()
+    {
+        boolean waitForExpectedStatus = true;
+        List<StepBase> workflow = CreateQuoteV2New();
+
+        /*List<StepBase> workflow = new ArrayList<StepBase>();
+
+        workflow.add(new LoadQuoteFilesAndExtractData());
+        workflow.add(new GetQuoteOAuthCredentials());
+        workflow.add(new QuoteCreate());
+        // workflow.add(new QuoteCreateV2New());
+        workflow.add(new QuoteStatus(waitForExpectedStatus));
+        workflow.add(new QuoteDetailsV2New());
+        workflow.add(new QuoteFinalizeV2New());
+        workflow.add(new QuoteStatusByQuoteNumber(waitForExpectedStatus));*/
         workflow.add(new QuoteUpdateOperationDelete());
 
         return workflow;
@@ -343,6 +360,8 @@ public class WorkflowLibrary
         return workflow;
     }
 
+
+
     public static List<StepBase> QuoteUpdateOperationInsertActionSwitchNeg()
     {
         boolean waitForExpectedStatus = true;
@@ -355,6 +374,19 @@ public class WorkflowLibrary
         // workflow.add(new QuoteCreateV2New());
         workflow.add(new QuoteStatus(waitForExpectedStatus));
         workflow.add(new QuoteDetailsV2New());
+        workflow.add(new QuoteUpdateOperationInsertActionSwitchNeg());
+
+        return workflow;
+    }
+
+    public static List<StepBase> QuoteUpdateOperationInsertActionSwitchSubNeg()
+    {
+        boolean waitForExpectedStatus = true;
+
+        List<StepBase> workflow = new ArrayList<StepBase>();
+
+        workflow.add(new LoadQuoteFilesAndExtractData());
+        workflow.add(new GetQuoteOAuthCredentials());
         workflow.add(new QuoteUpdateOperationInsertActionSwitchNeg());
 
         return workflow;
