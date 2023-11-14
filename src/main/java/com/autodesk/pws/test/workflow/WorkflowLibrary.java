cda560219b8ee7ccfbfd594593d194279bc5ac8a
pathads <pathads@ADSKPF3TJLM2>
2023-11-14 21:40:54 +0530

    Added new testcase Kicker.QuoteServices.Update.Quote.V2.Operation.Update.Action.New.Extra.Field.Negative.STG.json

--- src/main/java/com/autodesk/pws/test/workflow/WorkflowLibrary.java
+++ src/main/java/com/autodesk/pws/test/workflow/WorkflowLibrary.java
@@ -296,6 +296,23 @@ public class WorkflowLibrary
         return workflow;
     }
 
+    public static List<StepBase> QuoteUpdateOperationUpdateActionNewNewFieldNeg()
+    {
+        boolean waitForExpectedStatus = true;
+
+        List<StepBase> workflow = new ArrayList<StepBase>();
+
+        workflow.add(new LoadQuoteFilesAndExtractData());
+        workflow.add(new GetQuoteOAuthCredentials());
+       /* workflow.add(new QuoteCreate());
+        // workflow.add(new QuoteCreateV2New());
+        workflow.add(new QuoteStatus(waitForExpectedStatus));
+        workflow.add(new QuoteDetailsV2New());*/
+        workflow.add(new QuoteUpdateOperationUpdateActionNewNewFieldNeg());
+
+        return workflow;
+    }
+
     public static List<StepBase> QuoteUpdateOperationUpdateActionNewNeg()
     {
         boolean waitForExpectedStatus = true;
