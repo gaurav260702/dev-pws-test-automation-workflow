53f1df5a27b0aa7cf566e20767493ddd68da9b9c
pathads <pathads@ADSKPF3TJLM2>
2023-12-07 21:05:59 +0530

    Added new test case Kicker.QuoteServices.Update.Quote.V2.Operation.Insert.Action.Renewal.NZ.Positive.STG.json

--- src/main/java/com/autodesk/pws/test/workflow/WorkflowLibrary.java
+++ src/main/java/com/autodesk/pws/test/workflow/WorkflowLibrary.java
@@ -432,6 +432,23 @@ public class WorkflowLibrary
         return workflow;
     }
 
+    public static List<StepBase> QuoteUpdateOperationInsertActionRenewalNZ()
+    {
+        boolean waitForExpectedStatus = true;
+
+        List<StepBase> workflow = new ArrayList<StepBase>();
+
+        workflow.add(new LoadQuoteFilesAndExtractData());
+        workflow.add(new GetQuoteOAuthCredentials());
+        workflow.add(new QuoteCreate());
+        // workflow.add(new QuoteCreateV2New());
+        workflow.add(new QuoteStatus(waitForExpectedStatus));
+        workflow.add(new QuoteDetailsV2New());
+        workflow.add(new QuoteUpdateOperationInsertActionRenewalNZ());
+
+        return workflow;
+    }
+
     public static List<StepBase> QuoteUpdateOperationInsertActionRenewalNewFieldNeg()
     {
         boolean waitForExpectedStatus = true;
