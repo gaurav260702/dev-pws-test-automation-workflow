5816a8a9d83e8fd11490d4a970cab6d6830a03a5
pathads <pathads@ADSKPF3TJLM2>
2024-01-08 17:49:18 +0530

    Added new test case Validator.QuoteServices.Update.Quote.V2.Operation.Insert.Action.TrueUp.NZ.Positive.STG.json

--- src/main/java/com/autodesk/pws/test/workflow/WorkflowLibrary.java
+++ src/main/java/com/autodesk/pws/test/workflow/WorkflowLibrary.java
@@ -619,6 +619,23 @@ public class WorkflowLibrary
         return workflow;
     }
 
+    public static List<StepBase> QuoteUpdateOperationInsertActionTrueUpNZ()
+    {
+        boolean waitForExpectedStatus = true;
+
+        List<StepBase> workflow = new ArrayList<StepBase>();
+
+        workflow.add(new LoadQuoteFilesAndExtractData());
+        workflow.add(new GetQuoteOAuthCredentials());
+        workflow.add(new QuoteCreate());
+        // workflow.add(new QuoteCreateV2New());
+        workflow.add(new QuoteStatus(waitForExpectedStatus));
+        workflow.add(new QuoteDetailsV2New());
+        workflow.add(new QuoteUpdateOperationInsertActionTrueUpNZ());
+
+        return workflow;
+    }
+
     public static List<StepBase> QuoteUpdateOperationInsertActionTrueUpNewFieldNeg()
     {
         boolean waitForExpectedStatus = true;
