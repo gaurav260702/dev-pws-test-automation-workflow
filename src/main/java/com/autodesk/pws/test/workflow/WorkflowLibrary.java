9e2f6f3556f7d4feaa89359f61a3edb11306837f
pathads <pathads@ADSKPF3TJLM2>
2024-01-12 01:55:54 +0530

    Added new test case Kicker.QuoteServices.Update.Quote.V2.Operation.Update.Action.New.Updating.EndDate.NZ.Negative.STG.json

--- src/main/java/com/autodesk/pws/test/workflow/WorkflowLibrary.java
+++ src/main/java/com/autodesk/pws/test/workflow/WorkflowLibrary.java
@@ -350,6 +350,23 @@ public class WorkflowLibrary
         return workflow;
     }
 
+    public static List<StepBase> QuoteUpdateOperationUpdateActionNewUpdateEndDate()
+    {
+        boolean waitForExpectedStatus = true;
+
+        List<StepBase> workflow = new ArrayList<StepBase>();
+
+        workflow.add(new LoadQuoteFilesAndExtractData());
+        workflow.add(new GetQuoteOAuthCredentials());
+        workflow.add(new QuoteCreate());
+        // workflow.add(new QuoteCreateV2New());
+        workflow.add(new QuoteStatus(waitForExpectedStatus));
+        workflow.add(new QuoteDetailsV2New());
+        workflow.add(new QuoteUpdateOperationUpdateActionNewUpdateEndDate());
+
+        return workflow;
+    }
+
     public static List<StepBase> QuoteUpdateOperationUpdateActionNewNewFieldNeg()
     {
         boolean waitForExpectedStatus = true;
