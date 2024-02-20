e5f6de254d0e3f967df04072aa5a7f205f1238ce
bhandag <gaurav.bhandari@autodesk.com>
2024-02-20 13:04:14 +0530

    Debugging some left over issues

--- src/main/java/com/autodesk/pws/test/WpeTestAutomationApplication.java
+++ src/main/java/com/autodesk/pws/test/WpeTestAutomationApplication.java
@@ -1,6 +1,6 @@
 package com.autodesk.pws.test;
 
-import xjava.util.Arrays;
+import java.util.Arrays;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
